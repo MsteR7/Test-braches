26b5ef636c63a17c92530f90170bd7ea85dbe0d2
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2024-02-01 11:45:45 +0100

    main

---
+++ test.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** main.c
+** File description:
+** main
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("HEllR\n");
+    return 84;
+}
